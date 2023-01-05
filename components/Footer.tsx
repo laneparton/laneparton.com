f0ee45f19566ee8ec8e90a556289f5221817329c
Lane Parton <lane.parton@xylem.com>
2023-01-05 08:42:34 -0500

    Update copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,7 +15,7 @@ import styles from './styles.module.css'
 export const FooterImpl: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>Copyright 2023 {config.author}</div>
 
       <div className={styles.social}>
         {config.twitter && (
