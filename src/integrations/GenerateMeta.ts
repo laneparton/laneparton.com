cc4f543624150d1a3968614e8118b99cf90864c1
Lane Parton <lane.parton@xylem.com>
2023-11-30 14:58:08 -0500

    fix: Update packages

--- src/integrations/GenerateMeta.ts
+++ src/integrations/GenerateMeta.ts
@@ -6,42 +6,33 @@ import { remark } from 'remark';
 import strip from 'strip-markdown';
 
 async function markdownToPlainText(parsedMarkdown) {
-  // Only process the body of the document
   const result = await remark().use(strip).process(parsedMarkdown.content);
   return String(result.value);
 }
 
 async function generateDescription(plainText, openAiApiKey) {
-  const openAiClient = new OpenAI({
-    apiKey: openAiApiKey
-  });
+  const openAiClient = new OpenAI({ apiKey: openAiApiKey });
 
   return openAiClient.chat.completions.create({
     messages: [
-      {
-        role: "system",
-        content: "1. Never exceed the suggested length of 50-160 characters. 2. Output this with no extra words. 3. Do not output the title in the description.",
-      },
-      { 
-        role: "user", 
-        content: "What is a good meta description for this text:" + plainText
-      },
+      { role: "system", content: "1. Never exceed 50-160 characters. 2. No extra words. 3. Exclude title." },
+      { role: "user", content: `Generate a meta description for: ${plainText}` }
     ],
     model: "gpt-3.5-turbo",
   });
 }
 
-function addDescriptionToFile(filePath, description) {
+function updateFileWithDescription(filePath, description) {
   const file = fs.readFileSync(filePath, 'utf8');
   const doc = matter(file);
-  console.log(doc.data)
-  
+
   doc.data.description = description;
-  delete(doc.data.layout)
   const newContent = matter.stringify(doc.content, doc.data);
-  
   fs.writeFileSync(filePath, newContent);
-  console.log('The file has been updated with the new metadata:', filePath);
+}
+
+function filterMarkdownPages(pages) {
+  return Array.from(pages.keys()).filter(page => page.endsWith('.md'));
 }
 
 export function generateMeta({ openAiApiKey }) {
@@ -49,35 +40,29 @@ export function generateMeta({ openAiApiKey }) {
     name: "generate-meta",
     hooks: {
       "astro:build:setup": async ({ pages, logger }) => {
-
-        let mdPages = []
-        pages.forEach((value, key) => {
-          if (key.endsWith('.md')) {
-            mdPages.push(key)
-          }
-        });
+        const markdownPages = filterMarkdownPages(pages);
 
         try {
-          for (const filePath of mdPages) {
+          for (const filePath of markdownPages) {
             const markdownData = fs.readFileSync(filePath, { encoding: 'utf8' });
             const parsedMarkdown = matter(markdownData);
 
-            if(parsedMarkdown.data.description) {
-              logger.info(`Already has description`)
-              continue
+            if (parsedMarkdown.data.description) {
+              logger.info(`Description already present in ${filePath}`);
+              continue;
             }
 
             const plainTextData = await markdownToPlainText(parsedMarkdown);
             const metaDescriptionResponse = await generateDescription(plainTextData, openAiApiKey);
             const metaDescription = metaDescriptionResponse.choices[0].message.content;
 
-            addDescriptionToFile(filePath, metaDescription);
-            logger.info(`Updated file with meta description: ${filePath}`);
+            updateFileWithDescription(filePath, metaDescription);
+            logger.info(`Updated ${filePath} with new meta description.`);
           }
         } catch (error) {
-          logger.error('An error occurred:', error);
+          logger.error(`Error during description generation: ${error.message}`);
         }
-        logger.info("Integration completed.");
+        logger.info("Meta description generation completed.");
       }
     }
   };
